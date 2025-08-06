0e640d4bb3d921a2005b1ab5b513672b0bbbc287
Daniel Daduyo <denny.daduyo1@gmail.com>
2025-08-06 12:14:00 +0200

    decile plots

--- utils/correlations_utils.py
+++ utils/correlations_utils.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 from utils.thinkstats import jitter
 
 def plot_scatter(val1, val2, figsize=(12,8), xlabel="Feature 1", ylabel="Feature 2"):
@@ -23,3 +24,27 @@ def plot_scatter_with_jitter(
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.grid()
+
+def plot_decile(df, feature1, feature2, figsize=(12,8), alpha=0.2, xlabel="Feature 1", ylabel="Feature 2", color="C0"):
+    deciles = pd.qcut(df[feature1], 10, labels=False) + 1
+    df_groupby = df.groupby(deciles)
+    series_groupby = df_groupby[feature2]
+    
+    low = series_groupby.quantile(0.1)
+    median = series_groupby.quantile(0.5)
+    high = series_groupby.quantile(0.9)
+    
+    xs = df_groupby[feature1].median()
+
+    x_vals = xs.sort_index().values
+    y_low = low.sort_index().values
+    y_med = median.sort_index().values
+    y_high = high.sort_index().values
+    
+    plt.figure(figsize=figsize)
+    plt.fill_between(x_vals, y_low, y_high, alpha=alpha)
+    plt.plot(x_vals, y_med, color=color, label="Median")
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.grid()
+    plt.legend()
