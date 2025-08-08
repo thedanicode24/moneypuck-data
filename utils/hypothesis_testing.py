17b995ae65c02c0c6a1c655a079a078bdf86289c
Daniel Daduyo <denny.daduyo1@gmail.com>
2025-08-09 01:43:50 +0200

    regressione lineare completato (goals vs xgoals)

--- utils/hypothesis_testing.py
+++ utils/hypothesis_testing.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from utils.distribution_analysis import create_pmf_from_kde
 from utils.thinkstats import underride
-from scipy.stats import gaussian_kde
+from scipy.stats import gaussian_kde, permutation_test
 from empiricaldist import Pmf
 
 def fill_tail(pmf, observed, side, **options):
@@ -27,3 +27,17 @@ def plot_permutation_test(simulated, statistic, figsize=(12,8), xlabel="Statisti
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.grid()
+
+def permutation_diff_means(group1, group2, n_resamples=100000, alternative='two-sided'):
+    def diff_in_means(x, y):
+        return abs(np.mean(x) - np.mean(y))
+
+    result = permutation_test(
+        (group1, group2),
+        diff_in_means,
+        permutation_type='independent',
+        alternative=alternative,
+        n_resamples=n_resamples
+    )
+
+    return result
